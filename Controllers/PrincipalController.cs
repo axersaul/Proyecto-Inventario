b272f61f8e3775ba2b0b525a8ab8681000d79c4b
carlos ayala <hacker_milton_1990@hotmail.com>
2020-03-01 22:35:15 -0600

    Todo el proyecto Inicial

---
+++ Controllers/PrincipalController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proyecto.Models;
+
+namespace proyecto.Controllers
+{
+
+    public class PrincipalController : Controller
+    {
+        private storemyinventoryEntities db = new storemyinventoryEntities();
+        // GET: Principal
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Models.ViewModels.Usuario datos)
+        {
+            string siguienteVista="Index";
+            var busqueda = from p in db.usuario
+                           where p.nombre == datos.Nombre
+                           select p.contrasena;
+
+            try
+            {
+                if (busqueda.First().ToString() == datos.Contrasena)
+                {
+                    siguienteVista = "MainMenu";
+                }
+
+            }
+            catch (Exception e)
+            {
+            
+            }
+
+            
+            return View("Menu/Index");
+        }
+        [HttpGet]
+        public ActionResult MainMenu()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Inventory()
+        {
+            return View();
+        }
+          
+    }
+}
